8bc3e224543108553ecb1d0a1c7bef21e29b3587
MP <mp@gmail.com>
2021-03-03 15:32:37 +0530

    test commit f1

---
+++ src/test/java/com/mycompany/locationmanagementapi/it/LocationManagementControllerITest.java
@@ -0,0 +1,13 @@
+package com.mycompany.locationmanagementapi.it;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LocationManagementControllerITest {
+	
+//	@Autowired
+//	private MockMvc mockMvc;
+}
