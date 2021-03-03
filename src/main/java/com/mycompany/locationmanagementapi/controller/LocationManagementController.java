b404a9d6546556c5ada70e55deace8517b1d15c9
MP <mp@gmail.com>
2021-03-03 15:43:33 +0530

    commited by Ranjan

--- src/main/java/com/mycompany/locationmanagementapi/controller/LocationManagementController.java
+++ src/main/java/com/mycompany/locationmanagementapi/controller/LocationManagementController.java
@@ -39,6 +39,7 @@ public class LocationManagementController {
 	
 	@GetMapping("/locations")
 	public ResponseEntity<List<Location>> getAllLoctions(){
+		System.out.print("change made by Ranjan");
 		System.out.println("change by another dev");
 		System.out.println("adding new printline in getAllLocations in git");
 		List<Location> locations = null;
---
+++ src/main/java/com/mycompany/locationmanagementapi/controller/RanjanDev.java
@@ -0,0 +1,5 @@
+package com.mycompany.locationmanagementapi.controller;
+
+public class RanjanDev {
+
+}
