2a13389075c7279cbd0bf878e4e4498b4bbc2892
MP <mp@gmail.com>
2021-03-03 15:47:45 +0530

    mychanges

--- src/main/java/com/mycompany/locationmanagementapi/controller/LocationManagementController.java
+++ src/main/java/com/mycompany/locationmanagementapi/controller/LocationManagementController.java
@@ -39,7 +39,11 @@ public class LocationManagementController {
 	
 	@GetMapping("/locations")
 	public ResponseEntity<List<Location>> getAllLoctions(){
+
 		System.out.print("change made by Ranjan");
+
+		System.out.println("Changes made by Madhuri locally in fetaure branch 2");
+
 		System.out.println("change by another dev");
 		System.out.println("adding new printline in getAllLocations in git");
 		List<Location> locations = null;
